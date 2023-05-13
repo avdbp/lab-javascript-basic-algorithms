097591a5f5654f22639ccce571579898a0ae4fea
avdbp <alejandro.vdb@gmail.com>
2023-05-13 09:24:13 +0200

    Agregando instrucciones ejercicios

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // Iteration 1: Names and Input
+
+// Iteration 1: Names and Input
+// 1.1 Create a variable hacker1 with the driver's name.
+// 1.2 Print "The driver's name is XXXX".
+// 1.3 Create a variable hacker2 with the navigator's name.
+// 1.4 Print "The navigator's name is YYYY".
+
 let hacker1 = "Alejandro";
 console.log(`The driver's name is ${hacker1}.`);
 
@@ -6,6 +13,13 @@ let hacker2 = "Camila";
 console.log(`The navigator's name is ${hacker2}.`)
 
 // Iteration 2: Conditionals
+
+// Iteration 2: Conditionals
+// 2.1. Depending on which name is longer, print:
+// - The driver has the longest name, it has XX characters. or
+// - It seems that the navigator has the longest name, it has XX characters. or
+// - Wow, you both have equally long names, XX characters!.
+
 if(hacker1.length > hacker2.length) {
     console.log(`The driver has the longer name, it has ${hacker1.length}.`);
 }
@@ -18,6 +32,17 @@ else{
 
 
 // Iteration 3: Loops
+
+// 3.1 Print the characters of the driver's name, separated by space, and in capital letters, i.e., "J O H N".
+
+// 3.2 Print all the characters of the navigator's name in reverse order, i.e., "nhoJ".
+
+// 3.3 Depending on the lexicographic order of the strings, print:
+
+// The driver's name goes first.
+// Yo, the navigator goes first, definitely.
+// What?! You both have the same name?
+
 let hacker1UpperCase = hacker1.toUpperCase();
 let hacker1UpperCaseSeparate = "";
 for(let i = 0; i < hacker1UpperCase.length; i++){
